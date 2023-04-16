e5d65a20925a765a9ea941fc188aab4190788968
gunesevitan <gunesevitan@gmail.com>
2023-04-16 12:03:38 +0300

    transforms

---
+++ src/image_matching/transformations.py
@@ -0,0 +1,38 @@
+import cv2
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
+
+def create_feature_extraction_transforms(**transform_parameters):
+
+    """
+    Create transformation pipeline for feature extraction
+
+    Parameters
+    ----------
+    transform_parameters: dict
+        Dictionary of transform parameters
+
+    Returns
+    -------
+    transforms: dict
+        Transform pipeline for feature extraction
+    """
+
+    feature_extraction_transforms = A.Compose([
+        A.Resize(
+            height=transform_parameters['resize_height'],
+            width=transform_parameters['resize_width'],
+            interpolation=cv2.INTER_NEAREST,
+            always_apply=True
+        ),
+        A.Normalize(
+            mean=transform_parameters['normalize_mean'],
+            std=transform_parameters['normalize_std'],
+            max_pixel_value=transform_parameters['normalize_max_pixel_value'],
+            always_apply=True
+        ),
+        ToTensorV2(always_apply=True)
+    ])
+
+    return feature_extraction_transforms
