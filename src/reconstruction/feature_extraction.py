1acec0e7cd2faf8f5da0569792267c8c959b8b74
gunesevitan <gunesevitan@gmail.com>
2023-05-13 14:41:10 +0300

    feature extraction dataloader

--- src/reconstruction/feature_extraction.py
+++ src/reconstruction/feature_extraction.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader, SequentialSampler
 import timm
+
+import datasets
 
 
 def load_feature_extractor(model_name, pretrained, model_args):
@@ -31,11 +34,49 @@ def load_feature_extractor(model_name, pretrained, model_args):
         pretrained=pretrained,
         **model_args
     )
-    # Override model.head layer in transformer models so the output will be features
+    # Override the head layer in transformer models so the output will be features
     model.head = nn.Identity()
     model.eval()
 
     return model
+
+
+def prepare_dataloader(image_paths, transforms, batch_size, num_workers):
+
+    """
+    Prepare data loader for inference
+
+    Parameters
+    ----------
+    image_paths: list of shape (n_images)
+        List of image paths
+
+    transforms: dict
+        Transform pipeline
+
+    batch_size: int
+        Batch size of the data loader
+
+    num_workers: int
+        Number of workers of the data loader
+
+    Returns
+    -------
+    data_loader: torch.utils.data.DataLoader
+        Data loader
+    """
+
+    dataset = datasets.ImageDataset(image_paths=image_paths, transforms=transforms)
+    data_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=SequentialSampler(dataset),
+        pin_memory=False,
+        drop_last=False,
+        num_workers=num_workers
+    )
+
+    return data_loader
 
 
 def extract_features(inputs, model, pooling_type, device, amp):
