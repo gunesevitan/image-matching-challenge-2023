5eb34f1aaad148a45366e8ac4be625b6d4a56f96
gunesevitan <gunesevitan@gmail.com>
2023-05-19 11:25:53 +0300

    image pair dataset

--- src/reconstruction/datasets.py
+++ src/reconstruction/datasets.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 from torch.utils.data import Dataset
 
+import image_utilities
+
 
 class ImageDataset(Dataset):
 
@@ -35,7 +37,7 @@ class ImageDataset(Dataset):
 
         Returns
         -------
-        images: torch.FloatTensor of shape (channel, height, width) or numpy.ndarray of shape (height, width, channel)
+        image: torch.FloatTensor of shape (channel, height, width) or numpy.ndarray of shape (height, width, channel)
             Image tensor or array
         """
 
@@ -46,3 +48,62 @@ class ImageDataset(Dataset):
             image = self.transforms(image=np.array(image))['image'].float()
 
         return image
+
+
+class ImagePairDataset(Dataset):
+
+    def __init__(self, image_paths, image_pair_indices, transforms):
+
+        self.image_paths = image_paths
+        self.image_pair_indices = image_pair_indices
+        self.transforms = transforms
+
+    def __len__(self):
+
+        """
+        Get the length the dataset
+
+        Returns
+        -------
+        length: int
+            Length of the dataset
+        """
+
+        return len(self.image_pair_indices)
+
+    def __getitem__(self, idx):
+
+        """
+        Get the idxth element in the dataset
+
+        Parameters
+        ----------
+        idx: int
+            Index of the sample (0 <= idx < length of the dataset)
+
+        Returns
+        -------
+        image1: torch.FloatTensor of shape (channel, height, width)
+            First image tensor
+
+        image2: torch.FloatTensor of shape (channel, height, width)
+            Second image tensor
+        """
+
+        image1 = image_utilities.get_image_tensor(
+            image_path_or_array=str(self.image_paths[self.image_pair_indices[idx][0]]),
+            resize_shape=self.transforms['resize_shape'],
+            resize_longest_edge=self.transforms['resize_longest_edge'],
+            scale=self.transforms['scale'],
+            grayscale=self.transforms['grayscale']
+        )
+
+        image2 = image_utilities.get_image_tensor(
+            image_path_or_array=str(self.image_paths[self.image_pair_indices[idx][1]]),
+            resize_shape=self.transforms['resize_shape'],
+            resize_longest_edge=self.transforms['resize_longest_edge'],
+            scale=self.transforms['scale'],
+            grayscale=self.transforms['grayscale']
+        )
+
+        return image1, image2
